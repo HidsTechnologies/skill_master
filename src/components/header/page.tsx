42496037d7e4352d27e8bfaeddd6c29dbf576669
Kunal Dubey <xakep8@protonmail.com>
2023-11-28 20:57:15 +0530

    fixed build issue

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -102,11 +102,8 @@ function Appbar() {
                     <ul className="flex flex-col absolute top-0 px-2 py-2 font-normal w-max border-2 bg-white text-black rounded-b-lg">
                       {Courses.map((skills, i) => {
                         return (
-                          <Link href={skills.link}>
-                            <li
-                              key={i}
-                              className="py-2 px-3 hover:bg-blue-500 hover:text-white rounded-lg"
-                            >
+                          <Link href={skills.link} key={i}>
+                            <li className="py-2 px-3 hover:bg-blue-500 hover:text-white rounded-lg">
                               {skills.title}
                             </li>
                           </Link>
